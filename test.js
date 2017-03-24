4551fcfa3634c13a723ef9df3788ae42e42da00a
javajohnhub <joedw1978@gmail.com>
2017-03-24 15:28:34 -0400

    commit

--- test.js
+++ test.js
@@ -3,7 +3,12 @@
  */
 var apod = require('./src/lib/apod.js');
 var helioviewer = require('./src/lib/helioviewer.js');
-
+var asterank = require('./src/lib/asterank.js');
 //with specific date and tags - For apod all args are optional
 //apod('2017-11-13');
-helioviewer.getjp2image(date="2014-01-01T23:59:59Z", sourceId=14);
+//helioviewer.getjp2image(date="2014-01-01T23:59:59Z", sourceId=14);
+asterank(
+    query={"e": {"$lt": 0.1},
+        "i": {"$lt": 4},
+        "a": {"$lt": 1.5}},
+    limit=1)
