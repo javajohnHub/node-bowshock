94e4e87aaf1b6f873214ebd2ac689775b1c0ec85
javajohnhub <joedw1978@gmail.com>
2017-03-24 16:16:43 -0400

    trying to figure out name spacing

---
+++ index.js
@@ -0,0 +1,12 @@
+var bowshock = module.exports = {};
+bowshock.apod = require('./src/lib/apod.js');
+bowshock.asterank = require('./src/lib/asterank.js');
+bowshock.earth = require('./src/lib/earth.js');
+bowshock.helioviewer = require('./src/lib/helioviewer.js');
+bowshock.maas = require('./src/lib/maas.js');
+bowshock.patents = require('./src/lib/patents.js');
+bowshock.skymorph = require('./src/lib/skymorph.js');
+bowshock.techport = require('./src/lib/techport.js');
+
+
+
