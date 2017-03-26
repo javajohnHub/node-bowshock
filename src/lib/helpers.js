e09c7a971372b856875677f06a44adb676f127cb
javajohnhub <joedw1978@gmail.com>
2017-03-26 19:27:19 -0400

    removed comment

--- src/lib/helpers.js
+++ src/lib/helpers.js
@@ -10,7 +10,7 @@ dispatch_http_get: function(url){
     request(url, function (error, response, body) {
         console.log('error:', error); // Print the error if one occurred
         console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-        console.log('body:', body); // Print the HTML for the Google homepage.
+        console.log('body:', body);
         return body;
     });
 },
