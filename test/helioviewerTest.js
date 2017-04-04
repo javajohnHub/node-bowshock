e9a57386a2f4bbf8fd836eb3f2d52888ad81b7f6
javajohnhub <joedw1978@gmail.com>
2017-04-04 19:09:17 -0400

    added more tests

--- test/helioviewerTest.js
+++ test/helioviewerTest.js
@@ -1576,4 +1576,24 @@ describe('Helioviewer', function() {
         // so it() can no longer catch
 
     });
+
+    it('should return response code 200 with display provided downloadScreenshot()', function(done) {
+        setTimeout( function () {
+            // Called from the event loop, not it()
+            // So only the event loop could capture uncaught exceptions from here
+            try {
+                var r = helioviewer.downloadScreenshot(3240748);
+
+                done(); // success: call done with no parameter to indicate that it() is done()
+            } catch( e ) {
+                done( e ); // failure: call done with an error Object to indicate that it() failed
+            }
+            var file = fs.readFileSync("./3240748.png");
+            assert.pathExists(file.toString(), "File exists");
+        }, 100 );
+        // returns immediately after setting timeout
+        // so it() can no longer catch
+
+    });
 });
+
