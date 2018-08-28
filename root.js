2050d557f4d6cc394d9c3caf4c201981d1577bc6
John <joedw1978@gmail.com>
2018-08-27 23:47:56 -0400

    delete

--- root.js
+++
@@ -1,8 +0,0 @@
-let bowshock = require('./index');
-const {of } = require('rxjs');
-//with specific date and tags - For apod all args are optional
-bowshock.apod("2017-03-11").subscribe((data) => {
-    data.then((dta) => {
-        return dta
-    })
-});
