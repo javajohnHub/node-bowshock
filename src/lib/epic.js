87943409215e4eea87cd53325f5de4435d5a7d6c
John <joedw1978@gmail.com>
2018-09-09 12:17:24 -0400

    epic changes

--- src/lib/epic.js
+++ src/lib/epic.js
@@ -4,66 +4,58 @@ const helpers = require("./helpers");
 let epic = {
   natural() {
     let uri = "https://epic.gsfc.nasa.gov/api/natural";
-    return helpers.getJSON(uri, 'GET')
+    return helpers.getJSON(uri, "GET");
   },
 
   naturalAll() {
     let uri = "https://epic.gsfc.nasa.gov/api/natural/all";
-    return helpers.getJSON(uri, 'GET')
+    return helpers.getJSON(uri, "GET");
   },
 
   naturalDate(date) {
     let uri = `https://epic.gsfc.nasa.gov/api/natural/date/${date}`;
-    return helpers.getJSON(uri, 'GET')
+    return helpers.getJSON(uri, "GET");
   },
 
   naturalAvailable() {
     let uri = "https://epic.gsfc.nasa.gov/api/natural/available";
-    return helpers.getJSON(uri, 'GET')
+    return helpers.getJSON(uri, "GET");
   },
 
-  createNaturalImageLinks(arr, date) {
-    let linkArr = [];
-    arr.forEach(image => {
-      let img = `${image.image}.png`;
-      let archive = `https://epic.gsfc.nasa.gov/archive/natural/${date.year}/${
-        date.month
-      }/${date.day}/png/${img}`;
-      linkArr.push(archive);
-    });
-    return linkArr;
+  createNaturalImageLink(image, date) {
+    let img = `${image}.png`;
+    let archive = `https://epic.gsfc.nasa.gov/archive/natural/${date.year}/${
+      date.month
+    }/${date.day}/png/${img}`;
+    return archive;
   },
 
   enhanced() {
     let uri = "https://epic.gsfc.nasa.gov/api/enhanced";
-    return helpers.getJSON(uri, 'GET')
+    return helpers.getJSON(uri, "GET");
   },
 
   enhancedAll() {
     let uri = "https://epic.gsfc.nasa.gov/api/enhanced/all";
-    return helpers.getJSON(uri, 'GET')
+    return helpers.getJSON(uri, "GET");
   },
 
   enhancedDate(date) {
     let uri = `https://epic.gsfc.nasa.gov/api/enhanced/date/${date}`;
-    return helpers.getJSON(uri, 'GET')
+    return helpers.getJSON(uri, "GET");
   },
 
   enhancedAvailable() {
     let uri = "https://epic.gsfc.nasa.gov/api/enhyanced/available";
-    return helpers.getJSON(uri, 'GET')
+    return helpers.getJSON(uri, "GET");
   },
 
-  createEnhancedImageLinks(arr, date) {
-    let linkArr = [];
-    arr.forEach(image => {
-      let img = `${image.image}.png`;
-      let archive = `https://epic.gsfc.nasa.gov/archive/enhanced/${date.year}/${
-        date.month
-      }/${date.day}/png/${img}`;
-      linkArr.push(archive);
-    });
-    return linkArr;
+  createEnhancedImageLinks(image, date) {
+    let img = `${image.image}.png`;
+    let archive = `https://epic.gsfc.nasa.gov/archive/enhanced/${date.year}/${
+      date.month
+    }/${date.day}/png/${img}`;
+    return archive;
   }
 };
 
