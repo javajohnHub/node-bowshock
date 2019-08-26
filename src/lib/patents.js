d2c72ddeab40bb50ac5cc0e80a78aef7b3eee1bf
John Edwards <joedw1978@gmail.com>
2019-08-26 14:53:40 -0400

    fixes to patent api

--- src/lib/patents.js
+++ src/lib/patents.js
@@ -9,44 +9,10 @@ create jobs, and improve quality of life. This endpoint provides structured, sea
 let helpers = require('./helpers');
 
 
-function patents(object){
-    /*
-     HTTPs REQUEST
+function patents(){
+    let base_url = "https://data.nasa.gov/resource/nasa-patents.json";
 
-     GET https://api.nasa.gov/patents
-
-     QUERY PARAMETERS
-
-     Parameter	Type	Default	Description
-     query	string	None	Search text to filter results
-     concept_tags	bool	False	Return an ordered dictionary of concepts from the patent abstract
-     limit	int	all	number of patents to return
-     api_key	string	DEMO_KEY	api.data.gov key for expanded usage
-     EXAMPLE QUERY
-
-     https://api.nasa.gov/patents/content?query=temperature&limit=5&api_key=DEMO_KEY
-
-     */
-    let base_url = "https://api.nasa.gov/patents/content?";
-    let req_url = '';
-    if(!object){
-        req_url = base_url;
-    }else{
-        if(object.query){
-            base_url += "query=" + object.query + "&";
-        }
-        if(object.concept_tags){
-            base_url += "concept_tags=True" + "&";
-        }
-        if(object.limit){
-            base_url += "limit=" + object.limit + "&";
-
-        }
-    }
-
-    req_url = base_url + "api_key=" + helpers.nasa_api_key();
-
-    return helpers.getJSON(req_url, 'GET')
+    return helpers.getJSON(base_url, 'GET')
 }
 
 module.exports = patents;
